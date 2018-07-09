f409547529fc7f6736c525bb6900692c7266130c
Caroline Clark <carolineclark@users.noreply.github.com>
2018-07-09 17:13:15 +0200

    Beautiful new player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,20 +6,21 @@ public class PlayerController : MonoBehaviour {
     public int direction = RIGHT;
 
     private float jumpSpeed = 10f;
-    private Rigidbody2D rigidbody;
+    private Rigidbody2D rb;
     private SpriteRenderer spriteRenderer;
     private int layerMask;
     private Animator animator;
 
     private const int RIGHT = 1;
     private const int LEFT = -1;
+    private string RUNNING_ANIM = "Running";
 
     void Start () 
     {
         layerMask = 1 << Constants.GROUND_LAYER;
         spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
-        rigidbody = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
         EventManager.StartListening(Constants.FALLING_OBJECT_HIT_EVENT, DeathByFallingObject);
 	}
 
@@ -27,27 +28,24 @@ public class PlayerController : MonoBehaviour {
     {
         float x = Input.GetAxis(Constants.HORIZONTAL_AXIS);
         float y = Input.GetAxis(Constants.VERTICAL_AXIS);
-        rigidbody.velocity = new Vector2(x, y);
+        rb.velocity = new Vector2(x, y);
 
         if (isGrounded() && Input.GetButtonDown(Constants.JUMP)) {
-            rigidbody.velocity += new Vector2(0, jumpSpeed);
+            rb.velocity += new Vector2(0, jumpSpeed);
         }
 
         UpdateImage(x, y);
     }
 
     private void UpdateImage (float inputX, float inputY) {
-        float xspeed = rigidbody.velocity.x;
-        if (xspeed > 0) { direction = RIGHT; }
-        if (xspeed < 0) { direction = LEFT; }
-        this.transform.localScale = new Vector3(direction, 1, 1);
+        float xspeed = rb.velocity.x;
+        spriteRenderer.flipX = xspeed < 0;
+        bool running = CloseToZero(xspeed) || CloseToZero(inputX);
+        animator.SetBool(RUNNING_ANIM, running);
+    }
 
-        // TODO Bug with idle animation appearing at incorrect time.
-        // When we turn around the player briefly flashes to an idle animation
-        // seeming to suggest that inputX is not directly related to input
-        // but instead interpolated over time.
-        bool running = xspeed != 0 || inputX != 0;
-        animator.SetBool("Running", running);
+    private bool CloseToZero(float num) {
+        return System.Math.Abs(num) > 0.01f;
     }
 
     private bool isGrounded() 
