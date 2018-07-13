eabf9b44cc076d3f8bbcd29d2be077f528c3b930
aMuir <aaron.muir21@gmail.com>
2018-07-13 16:49:48 +0200

    flowers aint for keeping

---
+++ Assets/Scripts/TableFlowerInteraction.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableFlowerInteraction : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+		if (GameObject.Find("Held Flowers") != null){
+			GameObject player = GameObject.Find("Held Flowers");
+			player.GetComponent<SpriteRenderer>().enabled = false;
+			GameObject tableFlowers = GameObject.Find("TableFlowers");
+			tableFlowers.GetComponent<SpriteRenderer>().enabled = true;
+		}
+    }
+}
