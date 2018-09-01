9281903b52bd5cb288d617a4a24e1c10fd0adaa5
Liney <carolinejaneclark+liney@gmail.com>
2018-09-01 11:44:41 +0100

    Monster now created after walking through trigger, follows player

---
+++ Assets/Scripts/Monster/MonsterController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterController : MonoBehaviour {
+    Rigidbody2D rb;
+    GameObject player;
+    SpriteRenderer spriteRenderer;
+    private float runningSpeed1 = 7f;
+    private float runningSpeed2 = 10f;
+    private float maxMagnitude1 = 3f;
+    private float maxMagnitude2 = 5f;
+    private float jumpSpeed = 20f;
+    private float maxY = 3f;
+
+	void Start () {
+        player = GameObject.Find(Constants.PLAYER_TAG);
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+    void Update()
+    {
+        Vector2 pos = transform.position - player.transform.position;
+        if (pos.magnitude > maxMagnitude2)
+        {
+            Running(runningSpeed2);
+        }
+        else if (pos.magnitude > maxMagnitude1)
+        {
+            Running(runningSpeed1);
+        }
+
+        //animator.SetBool(RUNNING_ANIM_FLAG, (rb.velocity.magnitude > EPISLON));
+    }
+
+    private void Running(float speed)
+    {
+        if (transform.position.x > player.transform.position.x)
+        {
+            StepLeft(speed);
+        }
+        else if (transform.position.x < player.transform.position.x)
+        {
+            StepRight(speed);
+        }
+        if (transform.position.y < player.transform.position.y - maxY)
+        {
+            SingleJump();
+        }
+    }
+
+    private void StepRight(float speed)
+    {
+        spriteRenderer.flipX = false;
+        rb.velocity = new Vector2(speed, rb.velocity.y);
+        //animator.SetBool(RUNNING_ANIM_FLAG, (rb.velocity.magnitude > EPISLON));
+    }
+
+    private void StepLeft(float speed)
+    {
+        spriteRenderer.flipX = true;
+        rb.velocity = new Vector2(-speed, rb.velocity.y);
+        //animator.SetBool(RUNNING_ANIM_FLAG, (rb.velocity.magnitude > EPISLON));
+    }
+
+    private void SingleJump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+    }
+}
---
+++ Assets/Scripts/Monster/MonsterTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterTrigger : MonoBehaviour {
+    private static string CREATURE_NAME = "Creature";
+    private GameObject monster;
+    private Collider2D collider2D;
+
+	void Start () {
+        // child of the gameobject will be disabled.
+        monster = transform.GetChild(0).gameObject;
+        monster.SetActive(false);
+        collider2D = GetComponent<Collider2D>();
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals(Constants.PLAYER_TAG)) {
+            monster.SetActive(true);
+            collider2D.enabled = false;
+        }
+    }
+}
